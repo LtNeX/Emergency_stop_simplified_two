f4055d8d268815f7189cc622eb4fd7ed5002a6dd
LtNeX <46462089+LtNeX@users.noreply.github.com>
2023-10-07 19:23:56 +0200

    Update emergencystopsimplified.js

--- octoprint_emergencystopsimplified/static/js/emergencystopsimplified.js
+++ octoprint_emergencystopsimplified/static/js/emergencystopsimplified.js
@@ -1,16 +1,16 @@
 $(function() {
-    function emergencystopsimplifiedViewModel(parameters) {
+    function emergencystopsimplifiedtwoViewModel(parameters) {
         var self = this;
         self.settingsViewModel = parameters[0];
 
         self.onDataUpdaterPluginMessage = function(plugin, data) {
-            if (plugin !== "emergencystopsimplified") {
+            if (plugin !== "emergencystopsimplifiedtwo") {
                 console.log('Ignoring '+plugin);
                 return;
             }
 
             new PNotify({
-                title: 'Emergency stop simplified',
+                title: 'Emergency stop simplifiedtwo',
                 text: data.msg,
                 type: data.type,
                 hide: data.autoClose
@@ -24,7 +24,7 @@ $(function() {
     // information to the global variable OCTOPRINT_VIEWMODELS
     ADDITIONAL_VIEWMODELS.push([
         // This is the constructor to call for instantiating the plugin
-        emergencystopsimplifiedViewModel,
+        emergencystopsimplifiedtwoViewModel,
 
         // This is a list of dependencies to inject into the plugin, the order which you request
         // here is the order in which the dependencies will be injected into your view model upon
@@ -32,6 +32,6 @@ $(function() {
         ["settingsViewModel"],
 
         // Finally, this is the list of selectors for all elements we want this view model to be bound to.
-        ["#settings_plugin_emergencystopsimplified_form"]
+        ["#settings_plugin_emergencystopsimplifiedtwo_form"]
     ]);
 });
