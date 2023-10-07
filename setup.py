9227f7548d170ba27fd5f4d8a2fee33d523ab997
LtNeX <46462089+LtNeX@users.noreply.github.com>
2023-10-07 19:08:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "emergencystopsimplified"
+plugin_identifier = "emergencystopsimplifiedtwo"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_emergencystopsimplified"
+plugin_package = "octoprint_emergencystopsimplifiedtwo"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "Emergency stop simplified"
+plugin_name = "Emergency stop simplified-two"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "0.1.1"
